ebe842ea5986dd55bcb6950e99309f354b974010
Sofiia Hoppova <sofiahoppova@gmail.com>
2025-02-20 15:13:49 +0200

    Add some styles. Update events. Add modal window with event details

--- modules/src/components/Calendar/Calendar.jsx
+++ modules/src/components/Calendar/Calendar.jsx
@@ -1,92 +1,130 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import FullCalendar from "@fullcalendar/react";
-import dayGridPlugin from "@fullcalendar/daygrid"; // a plugin!
+import dayGridPlugin from "@fullcalendar/daygrid";
 import timeGridPlugin from "@fullcalendar/timegrid";
 import interactionPlugin from "@fullcalendar/interaction";
 
+import { API } from "../../shared/api";
+
+import Box from "@mui/material/Box";
+import Modal from "@mui/material/Modal";
+import "../Calendar/Calendar.css";
+
 const Calendar = () => {
-  const [isEdit, setIsEdit] = useState(false);
-  const [selectedSlots, setSelectedSlots] = useState([]);
+  const [openModal, setOpenModal] = useState(false);
+  const [students, setStudents] = useState([]);
+  const [events, setEvents] = useState([
+    { id: "1", title: "High Five", start: "2025-02-17T12:00:00+02:00" },
+    { id: "2", title: "Golden Eagles", start: "2025-02-17T17:00:00" },
+    { id: "3", title: "Crazy Frogs", start: "2025-02-18T14:00:00" },
+    { id: "5", title: "Bright Stars", start: "2025-02-19T16:00:00" },
+    { id: "6", title: "Happy Pandas", start: "2025-02-19T11:00:00" },
+    { id: "7", title: "High Five", start: "2025-02-20T12:00:00" },
+    { id: "8", title: "Golden Eagles", start: "2025-02-20T17:00:00" },
+    { id: "9", title: "Crazy Frogs", start: "2025-02-21T14:00:00" },
+    { id: "11", title: "Bright Stars", start: "2025-02-22T16:00:00" },
+    { id: "12", title: "Happy Pandas", start: "2025-02-22T11:00:00" },
+  ]);
+  const [selectedEvent, setSelectedEvent] = useState(null);
 
-  // const handleClick= () => {
-  //     setIsEdit(!isEdit);
-  // }
+  useEffect(() => {
+    const fetchData = async () => {
+      const { data } = await axios.get(`http://localhost:4000${API.students}`);
+      setStudents(data);
+      console.log(data);
+    };
+    fetchData();
+  }, []);
 
-  const handleSelect = (selectionInfo) => {
-    console.log("one slote choosed");
-    const start = selectionInfo.startStr.slice(11, 16); // Витягуємо годину (HH:MM)
-    const end = selectionInfo.endStr.slice(11, 16);
-    const day = selectionInfo.start.toLocaleDateString("en-US", {
-      weekday: "long",
-    });
-    console.log(start, end, day);
-
-    setSelectedSlots((prev) => [...prev, { day, time: `${start}-${end}` }]);
-    console.log(selectedSlots);
+  // Обробка кліку на подію
+  const handleEventClick = (clickInfo) => {
+    setSelectedEvent(clickInfo.event);
+    setOpenModal(true);
+  };
+  // Обробка перетягування подій
+  const handleEventDrop = (info) => {
+    const updatedEvents = events.map((event) =>
+      event.id === info.event.id
+        ? { ...event, start: info.event.startStr }
+        : event
+    );
+    setEvents(updatedEvents);
   };
 
-  const timeSlots = [
-    [
-      "12:00-13:00",
-      "13:00-14:00",
-      "14:00-15:00",
-      "15:00-16:00",
-      "16:00-17:00",
-      "17:00-18:00",
-    ],
-  ];
+  const handleCloseModal = () => setOpenModal(false);
 
-  //   const filteredSlots = selectedSlots.filter(({ time }) =>
-  //     timeSlots[0].includes(time)
-  //   );
+  const formatDate = (date) => {
+    if (date) {
+      const options = { hour: "2-digit", minute: "2-digit" };
+      return date.toLocaleTimeString("uk-UA", options);
+    }
+    return "час не вказано";
+  };
 
   return (
-    <div>
+    <div className="">
       <h3 className="text-[32px]">Календар занять</h3>
-      {/* 
-      <button
-        type="button"
-        onClick={() => setIsEdit(!isEdit)}
-        className={
-          "bg-emerald-500 text-white mb-4 rounded-lg px-8 py-2 transition duration-300 ease-in-out hover:bg-emerald-600 shadow-lg shadow-emerald-500/50"
-        }
-      >
-        Редагувати
-      </button> */}
       <FullCalendar
-        plugins={[timeGridPlugin, dayGridPlugin, interactionPlugin]}
-        initialView="dayGridMonth"
+        plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+        initialView="timeGridWeek"
+        viewClassNames="h-[582px] p-[24px] bg-white shadow-lg rounded-xl"
         slotMinTime="12:00:00"
         slotMaxTime="18:00:00"
+        slotDuration="00:20"
         slotLabelInterval="01:00"
-        slotDuration="01:00"
-        viewClassNames="h-[500px] bg-white p-[24px] rounded-xl"
-        dayCellClassNames={""}
-        slotLaneClassNames={"h-[50px]"}
-        // eventClassNames={"bg-pink-400"}
-        // moreLinkClassNames={"bg-emerald-400"}
-
-        dayHeaderClassNames={"bg-emerald-400"}
-        slotLabelClassNames={"bg-emerald-400 font-[24px]"}
-        allDayClassNames="border-emerald-600"
-        weekNumberClassNames={"bg-emerald-400"}
-        nowIndicatorClassNames={"bg-yellow-400"}
+        editable={true} // Дозволяє змінювати події
+        droppable={true} // Дозволяє перетягування
+        eventDrop={handleEventDrop} // Викликається після перетягування
+        events={events} // Передаємо масив подій
+        eventClick={handleEventClick} // Викликається після click
+        nowIndicatorClassNames="bg-emerald-400 text-pink-500"
         headerToolbar={{
-          left: "prev,next today", // Navigation buttons
+          left: "prev today",
           center: "title",
-          right: "dayGridMonth,timeGridWeek,timeGridDay", // Buttons for views
+          right: "dayGridMonth,timeGridWeek,timeGridDay next",
         }}
-        selectable={isEdit}
-        select={handleSelect}
-        themeSystem="bootstrap5"
       />
+      <div>
+        <Modal
+          open={openModal}
+          onClose={handleCloseModal}
+          aria-labelledby="modal-modal-title"
+          aria-describedby="modal-modal-description"
+        >
+          <Box className="absolute top-[50%] left-[50%] w-[350px] -translate-x-1/2 -translate-y-1/2 bg-white rounded-xl p-5 ">
+            <h3 className="text-center text-[24px] mb-[10px]">
+              Деталі заняття
+            </h3>
+            <h4 className="text-[18px]">
+              Назва групи:{" "}
+              <span className="text-violet-800 font-bold">
+                {selectedEvent?._def?.title}
+              </span>
+            </h4>
+            <p className="text-gray-500 mb-[8px]">
+              Час виділений на урок:{" "}
+              <span>
+                {formatDate(selectedEvent?._instance?.range?.start)} -{" "}
+                {formatDate(selectedEvent?._instance?.range?.end)}{" "}
+              </span>
+            </p>
+            <h4 className="text-violet-800 m-auto w-fit mb-[8px]">Учасники</h4>
+            <ul className="flex flex-col gap-[6px]">
+              {students.map((student) => (
+                <li key={student.id} className="">
+                  <p>
+                    {student.firstName} {student.lastName}
+                  </p>
+                  <span className="block h-[1px] bg-gray-300" />
+                </li>
+              ))}
+            </ul>
+          </Box>
+        </Modal>
+      </div>
     </div>
   );
 };
 
 export default Calendar;
-// selectedSlots.map(({ day, time }) => ({
-//         title: "Вільний слот",
-//         start: `2024-02-19T${time.split("-")[0]}:00`,
-//         end: `2024-02-19T${time.split("-")[1]}:00`,
-//       }))
--- modules/src/routes/AppRoutes.jsx
+++ modules/src/routes/AppRoutes.jsx
@@ -1,8 +1,9 @@
 import { Routes, Route } from "react-router-dom";
+
+import LandingPage from "../pages/LandingPage";
 import Login from "../pages/Login";
 import Register from "../pages/Register";
 import PrivateRoute from "../components/PrivateRoute";
-import LandingPage from "../pages/LandingPage";
 import { Dashboard } from "../components/Dashboard";
 import { StudentsList } from "../components/StudentsList/StudentsList";
 import { ProfilePage } from "../components/Profile";
@@ -16,21 +17,14 @@ const AppRoutes = () => {
       <Route path="/" element={<LandingPage />} />
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
-      {/* <Route
-        path="/list"
-        element={
-          <Dashboard>
-            <StudentsList />
-          </Dashboard>
-        }
-      /> */}
+
+      {/* Приватні маршрути */}
       <Route path="/dashboard" element={<Dashboard />}>
         <Route path="groups" element={<StudentsList />} />
         <Route path="schedule" element={<Schedule />} />
         <Route path="profile" element={<ProfilePage />} />
         <Route path="support" element={<Support />} />
       </Route>
-      {/* Приватні маршрути */}
       <Route element={<PrivateRoute />}></Route>
     </Routes>
   );
