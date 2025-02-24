821f00527fb5b8c448af79f7c904961a3b008369
Yura Nagorniak <yurii.nagorniak@gmail.com>
2025-02-24 01:37:26 +0000

    add generating groups

--- modules/src/shared/api/api-routes.js
+++ modules/src/shared/api/api-routes.js
@@ -3,9 +3,14 @@ const AUTH_ROUTES = {
     register: "/auth/register"
 }
 
+const GROUP_ROUTES = {
+    base: "/study-groups",
+    generate: "/study-groups/generate",
+}
+
 export const API_ROUTES = {
     auth: AUTH_ROUTES,
+    groups: GROUP_ROUTES,
     teachers: "/teachers",
     students: "/students",
-    groups: "/study-groups"
 }
