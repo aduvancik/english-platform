82afef37d2cf9b38b1d237cff86a61fadf34c322
Yura Nagorniak <yurii.nagorniak@gmail.com>
2025-03-05 23:38:45 +0000

    add AddStudentToGroupModal

--- modules/src/components/StudentRequests/StudentRequests.jsx
+++ modules/src/components/StudentRequests/StudentRequests.jsx
@@ -1,30 +1,41 @@
-import { useState } from "react";
+import { useState, useContext } from "react";
 import { StudentRequestCard } from "../../shared/layout";
 import { LabelledAddUserButton } from "../../shared/ui";
 import { StudentInfoModal } from "./StudentInfoModal";
+import { AddStudentToGroupModal } from "./AddStudentToGroupModal";
+import { StudentContext } from "../../contexts/StudentContextProvider/StudentContext";
 
-export const StudentRequests = ({ students, groupsGenerated }) => {
-    const [openModal, setOpenModal] = useState(false);
+export const StudentRequests = ({ ref }) => {
+    const { students, groupsGenerated } = useContext(StudentContext);
+
     const [selectedStudent, setSelectedStudent] = useState({});
-    const handleOpenModal = () => setOpenModal(true);
-    const handleCloseModal = () => setOpenModal(false);
+
+    const [openStudentInfoModal, setOpenStudentInfoModal] = useState(false);
+    const handleOpenStudentInfoModal = () => setOpenStudentInfoModal(true);
+    const handleCloseStudentInfoModal = () => setOpenStudentInfoModal(false);
+
+    const [openAddStudentModal, setOpenAddStudentModal] = useState(false);
+    const handleOpenAddStudentModal = () => setOpenAddStudentModal(true);
+    const handleCloseAddStudentModal = () => setOpenAddStudentModal(false);
 
     const renderedStudents = students
         .filter((student) => !groupsGenerated || (groupsGenerated && !student.studyGroup))
         .map((student) => (
             <StudentRequestCard
                 key={student.id}
+                id={student.id}
                 name={student.firstName}
                 surname={student.lastName}
                 languageLevel={student.languageLevel.name}
-                openModal={handleOpenModal}
+                openStudentInfoModal={handleOpenStudentInfoModal}
+                openAddStudentModal={handleOpenAddStudentModal}
                 selectStudent={() => setSelectedStudent(student)}
             />
         ));
 
     return (
         <>
-            <div className="flex flex-col gap-3">
+            <div className="flex flex-col gap-3" ref={ref}>
                 <div className="flex justify-between">
                     <h3 className="text-[20px]">Нові запити ({renderedStudents.length})</h3>
                     <LabelledAddUserButton label={"Додати до групи"}></LabelledAddUserButton>
@@ -34,7 +45,18 @@ export const StudentRequests = ({ students, groupsGenerated }) => {
                 </ul>
             </div>
             <div>
-                <StudentInfoModal openModal={openModal} handleCloseModal={handleCloseModal} student={selectedStudent}/>
+                <StudentInfoModal
+                    openModal={openStudentInfoModal}
+                    handleCloseModal={handleCloseStudentInfoModal}
+                    student={selectedStudent}
+                />
+            </div>
+            <div>
+                <AddStudentToGroupModal
+                    openModal={openAddStudentModal}
+                    handleCloseModal={handleCloseAddStudentModal}
+                    student={selectedStudent}
+                />
             </div>
         </>
     );
