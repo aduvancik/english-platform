63fb6c874e3a39b580f22be00f5b4613ef360705
Yura Nagorniak <yurii.nagorniak@gmail.com>
2025-03-05 23:37:14 +0000

    fix AddUserButton

--- modules/src/shared/ui/AddUserButton.jsx
+++ modules/src/shared/ui/AddUserButton.jsx
@@ -1,8 +1,14 @@
 import { AddUserIcon } from "../icons";
 
 export const AddUserButton = ({ color, onClick = () => {} }) => {
+
+    const handleClick = (e) => {
+        e.stopPropagation();
+        onClick();
+    }
+
     return (
-        <button onClick={onClick}  className="bg-transparent">
+        <button onClick={handleClick}  className="bg-transparent">
             <AddUserIcon color={color} />
         </button>
     );
